be2be18e058cfa60a8fffb0c0203994e1d50c37e
Tapasya Patki <patki1@llnl.gov>
2012-07-01 16:34:03 -0700

    Editing the print function to write to a file

--- src/components/rapl/print-utils/rapl_print.c
+++ src/components/rapl/print-utils/rapl_print.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "rapl_print.h"
 
-void rapl_print_info(int *code, int num_events, double elapsed, long long *values){
+void rapl_print_info(int *code, int num_events, double elapsed, long long *values, int uid){
 	
 	int i=0;
 	int r; 
@@ -11,67 +12,135 @@ void rapl_print_info(int *code, int num_events, double elapsed, long long *value
     	char units[MAX_RAPL_EVENTS][PAPI_MIN_STR_LEN];
 	PAPI_event_info_t evinfo;
 	int retval;
-
-	for(i=0;i<num_events;i++) {
-	        retval = PAPI_event_code_to_name(code[i], event_names[i] );
-	        if ( retval != PAPI_OK ) {
+	char hostname[1025];
+	char filetag[2048];
+	FILE *fp=NULL;
+	
+	gethostname(hostname, 1024);
+	sprintf(filetag, "%s_rapl_%d.out", hostname, uid);
+	fp = fopen(filetag, "w");
+	if(fp==NULL){
+		fprintf(stderr, "Error: Could not open file %s. Printing to screen.", filetag);
+		
+		for(i=0;i<num_events;i++) {
+		        retval = PAPI_event_code_to_name(code[i], event_names[i] );
+	       		 if ( retval != PAPI_OK ) {
         		   printf("Error translating %x\n",code[i]);
            		   test_fail( __FILE__, __LINE__, "PAPI_event_code_to_name", retval );
-       		 }
+       		 	}
 
-       		 retval = PAPI_get_event_info(code[i],&evinfo);
-        	if (retval != PAPI_OK) {
-          		test_fail( __FILE__, __LINE__,"Error getting event info\n",retval);
+       		 	retval = PAPI_get_event_info(code[i],&evinfo);
+        		if (retval != PAPI_OK) {
+          			test_fail( __FILE__, __LINE__,"Error getting event info\n",retval);
+        		}
+
+        		strncpy(units[i],evinfo.units,PAPI_MIN_STR_LEN);
+
+		}
+
+		printf("\n*************** BEGIN RAPL INFO ***************");
+
+        	printf("\nEnergy measurements:\n");
+
+		for(i=0;i<num_events;i++) {
+           		if (strstr(units[i],"nJ")) {
+
+              		printf("%s\t%.6fJ\t(Average Power %.1fW)\n",
+                    		event_names[i],
+                    		(double)values[i]/1.0e9,
+                    		((double)values[i]/1.0e9)/elapsed);
+           		}
         	}
 
-        	strncpy(units[i],evinfo.units,PAPI_MIN_STR_LEN);
+        	printf("\n");
+       		printf("Fixed values:\n");
 
-	}
+        	for(i=0;i<num_events;i++) {
+	   		if(strstr(units[i],"R")){
+				printf("%s\t0x%x\n", event_names[i], values[i]);
+	   		}
+           	if (!strstr(units[i],"nJ") && !strstr(units[i],"R")) {
 
-	printf("\n*************** BEGIN RAPL INFO ***************");
+             	union {
+               		long long ll;
+               		double fp;
+             	} result;
 
-        printf("\nEnergy measurements:\n");
+             	result.ll=values[i];
 
-	for(i=0;i<num_events;i++) {
-           if (strstr(units[i],"nJ")) {
+              	printf("%s\t%0.3f%s\n",
+                	    event_names[i],
+                    		result.fp,
+                    		units[i]);
+         	}
+ 	       }
 
-              printf("%s\t%.6fJ\t(Average Power %.1fW)\n",
-                    event_names[i],
-                    (double)values[i]/1.0e9,
-                    ((double)values[i]/1.0e9)/elapsed);
-           }
-        }
+		printf("\n*************** END RAPL INFO ***************\n");
+	} /*End of if(fp==NULL) */
+	
+	else { /*Write to a file */
+	
+	
+		for(i=0;i<num_events;i++) {
+		        retval = PAPI_event_code_to_name(code[i], event_names[i] );
+	       		 if ( retval != PAPI_OK ) {
+        		   printf("Error translating %x\n",code[i]);
+           		   test_fail( __FILE__, __LINE__, "PAPI_event_code_to_name", retval );
+       		 	}
 
-        printf("\n");
-        printf("Fixed values:\n");
+       		 	retval = PAPI_get_event_info(code[i],&evinfo);
+        		if (retval != PAPI_OK) {
+          			test_fail( __FILE__, __LINE__,"Error getting event info\n",retval);
+        		}
 
-        for(i=0;i<num_events;i++) {
-	   if(strstr(units[i],"R")){
-		printf("%s\t0x%x\n", event_names[i], values[i]);
+        		strncpy(units[i],evinfo.units,PAPI_MIN_STR_LEN);
+
+		}
+
+		fprintf(fp,"\n*************** BEGIN RAPL INFO ***************");
+
+        	fprintf(fp,"\nEnergy measurements:\n");
+
+		for(i=0;i<num_events;i++) {
+           		if (strstr(units[i],"nJ")) {
+
+              		fprintf(fp,"%s\t%.6fJ\t(Average Power %.1fW)\n",
+                    		event_names[i],
+                    		(double)values[i]/1.0e9,
+                    		((double)values[i]/1.0e9)/elapsed);
+           		}
+        	}
+
+        	fprintf(fp,"\n");
+       		fprintf(fp,"Fixed values:\n");
+
+        	for(i=0;i<num_events;i++) {
+	   		if(strstr(units[i],"R")){
+				fprintf(fp,"%s\t0x%x\n", event_names[i], values[i]);
+	   		}
+           	if (!strstr(units[i],"nJ") && !strstr(units[i],"R")) {
+
+             	union {
+               		long long ll;
+               		double fp;
+             	} result;
+
+             	result.ll=values[i];
+
+              	fprintf(fp,"%s\t%0.3f%s\n",
+                	    event_names[i],
+                    		result.fp,
+                    		units[i]);
+
+		}
+
 	   }
-           if (!strstr(units[i],"nJ") && !strstr(units[i],"R")) {
-
-             union {
-               long long ll;
-               double fp;
-             } result;
-
-             result.ll=values[i];
-
-              printf("%s\t%0.3f%s\n",
-                    event_names[i],
-                    result.fp,
-                    units[i]);
-           }
-        }
-
-	printf("\n*************** END RAPL INFO ***************\n");
+	fclose(fp);
+   }/*End of else*/
 }
 
 
-
-
-void rapl_print_info_event_names(char event_names[MAX_RAPL_EVENTS][PAPI_MAX_STR_LEN], int num_events, double elapsed, long long *values){
+void rapl_print_info_event_names(char event_names[MAX_RAPL_EVENTS][PAPI_MAX_STR_LEN], int num_events, double elapsed, long long *values, int uid){
 	
 	int i=0;
 	int r; 
--- src/components/rapl/print-utils/rapl_print.h
+++ src/components/rapl/print-utils/rapl_print.h
@@ -6,7 +6,7 @@
 /*Not exported externally so need to redefine it here*/
 #define MAX_RAPL_EVENTS 64
 
-void rapl_print_info(int *code, int num_events, double elapsed, long long *values);
-void rapl_print_info_event_names(char event_names[MAX_RAPL_EVENTS][PAPI_MAX_STR_LEN], int num_events, double elapsed, long long *values);
+void rapl_print_info(int *code, int num_events, double elapsed, long long *values, int uid);
+void rapl_print_info_event_names(char event_names[MAX_RAPL_EVENTS][PAPI_MAX_STR_LEN], int num_events, double elapsed, long long *values, int uid);
 
 #endif 
