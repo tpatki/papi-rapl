1b5a4fd79fdfb2894cb24646a95c8f72f57bb01d
Tapasya Patki <patki1@llnl.gov>
2012-07-01 23:42:24 -0700

    Extended

--- src/components/rapl/print-utils/rapl_print.c
+++ src/components/rapl/print-utils/rapl_print.c
@@ -155,6 +155,21 @@ void rapl_print_info_event_names(char event_names[MAX_RAPL_EVENTS][PAPI_MAX_STR_
     	char units[MAX_RAPL_EVENTS][PAPI_MIN_STR_LEN];
 	PAPI_event_info_t evinfo;
 	int retval;
+	FILE *fp=NULL;
+	char hostname[1025];
+        char filetag[2048];
+
+
+
+	gethostname(hostname, 1024);
+        sprintf(filetag, "%s_rapl_%d.out", hostname, uid);
+
+        fp = fopen(filetag, "w");
+
+
+	if(fp == NULL) { /*Print to the screen */
+	
+	  fprintf(stderr, "Error: Could not open file %s. Printing to screen.", filetag);
 
 	for(i=0;i<num_events;i++) {
 	        retval = PAPI_event_name_to_code(event_names[i],&code );
@@ -210,5 +225,64 @@ void rapl_print_info_event_names(char event_names[MAX_RAPL_EVENTS][PAPI_MAX_STR_
         }
 
 	printf("\n*************** END RAPL INFO ***************\n");
-}
+   } /*End of if(fp==NULL) */
 
+else { 
+
+	for(i=0;i<num_events;i++) {
+	        retval = PAPI_event_name_to_code(event_names[i],&code );
+	        if ( retval != PAPI_OK ) {
+        		   printf("Error translating %x\n",code);
+           		   test_fail( __FILE__, __LINE__, "PAPI_event_code_to_name", retval );
+       		 }
+
+       		 retval = PAPI_get_event_info(code,&evinfo);
+        	if (retval != PAPI_OK) {
+          		test_fail( __FILE__, __LINE__,"Error getting event info\n",retval);
+        	}
+
+        	strncpy(units[i],evinfo.units,PAPI_MIN_STR_LEN);
+
+	}
+
+	fprintf(fp,"\n*************** BEGIN RAPL INFO ***************");
+
+        fprintf(fp,"\nEnergy measurements:\n");
+
+	for(i=0;i<num_events;i++) {
+           if (strstr(units[i],"nJ")) {
+
+              fprintf(fp,"%s\t%.6fJ\t(Average Power %.1fW)\n",
+                    event_names[i],
+                    (double)values[i]/1.0e9,
+                    ((double)values[i]/1.0e9)/elapsed);
+           }
+        }
+
+        fprintf(fp,"\n");
+        fprintf(fp,"Fixed values:\n");
+
+        for(i=0;i<num_events;i++) {
+	   if(strstr(units[i],"R")){
+		fprintf(fp,"%s\t0x%x\n", event_names[i], values[i]);
+	   }
+           if (!strstr(units[i],"nJ") && !strstr(units[i],"R")) {
+
+             union {
+               long long ll;
+               double fp;
+             } result;
+
+             result.ll=values[i];
+
+              fprintf(fp,"%s\t%0.3f%s\n",
+                    event_names[i],
+                    result.fp,
+                    units[i]);
+           }
+        }
+
+	fprintf(fp,"\n*************** END RAPL INFO ***************\n");
+
+} /*End of else */
+}
